e9b1f1823f4fb878c545f7b728c33bbbf9c21210
WeepingDogel <weepingdogel@gmail.com>
2021-06-15 11:14:57 +0800

    数学运算

--- C/count.c
+++ C/count.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
+#include<math.h>
+#define PI 3.14
 
 int main(){
-    
+    float x,y;
+    printf("please type the value of x:");
+    scanf("%f",&x);
+    if(x < 2 * -(PI/2)){
+        y = 2 * pow(x,3) + 3 * cos(x) + 5;
+        printf("%.4f",y);
+    }
+    if (x >= 2 * -(PI/2) && x < PI/2)
+    {
+        y = pow((x -1)/(x+2),3) + 5 * x;
+        printf("%.4f",y);
+    }
+    if(x >= PI/2)
+    {
+        y = sqrt(x + sin(3*x));
+        printf("%.4f",y);
+    }
+    return 0;
 }
