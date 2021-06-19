7499bedf239b8f45fac680893d2dee380aab6ce4
WeepingDogel <weepingdogel@gmail.com>
2021-06-19 21:48:15 +0800

    修改代码细节

--- C/count.c
+++ C/count.c
@@ -11,11 +11,11 @@ int main(){
     float x,y;
     printf("please type the value of x:");
     scanf("%f",&x);
-    if(x < 2 * -(PI/2)){
+    if(x < -(PI/2)){
         y = 2 * pow(x,3) + 3 * cos(x) + 5;
         printf("%.4f",y);
     }
-    if (x >= 2 * -(PI/2) && x < PI/2)
+    if (x >= -(PI/2) && x < PI/2)
     {
         y = pow((x -1)/(x+2),3) + 5 * x;
         printf("%.4f",y);
